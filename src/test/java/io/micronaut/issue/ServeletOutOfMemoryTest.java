2a7479fb02858f522e0732847a09e058a99942d3
Akshay Vadher <akshayvadher>
2021-08-11 16:05:52 +0000

    Initial commit

---
+++ src/test/java/io/micronaut/issue/ServeletOutOfMemoryTest.java
@@ -0,0 +1,21 @@
+package io.micronaut.issue;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class ServeletOutOfMemoryTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
